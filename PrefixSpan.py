d1ea5779a21ee26b11bb3e3e84f1fbcf038aa0c4
Yi-Hsun Liu <lyharthur@gmail.com>
2016-08-01 16:00:36 +0800

    Update PrefixSpan.py

--- PrefixSpan.py
+++ PrefixSpan.py
@@ -2,20 +2,57 @@ from collections import defaultdict, namedtuple
 
 
 
-def find_frequent_item(db,min_supprot):
-    items = defaultdict(lambda: 0)
+def find_frequent_item(db,min_supprot,prefix_object):
+    prefix = defaultdict(lambda: 0)
+    db_new = []
+    #print(prefix_object)
     for sequence in db:
         flattened  = [val for sublist in sequence for val in sublist]
         for item in set(flattened):
-            items[item]+=1
-    items = dict((item, support) for item, support in list(items.items())
+            if item != '_':
+                prefix[item]+=1
+    prefix = dict((item, support) for item, support in list(prefix.items())
         if support >= min_supprot)
     for sequence in db:
         for itemset in sequence:
             for item in itemset:
-                if item not in items:
+                if item not in prefix:
                     itemset.remove(item)
-    return db
+        db_new.append(list(filter(None, sequence)))
+        
+    for prefix_item in prefix:
+        print(prefix_item)
+        db_object= []
+        for seq in db_new:
+            seq_new = []
+            a=0
+            for itemset in seq:    
+                itemset_n = itemset[:]
+                if a==0:
+                    for item in itemset:
+                        if item == prefix_item:
+                            itemset_n[itemset_n.index(item)] = '_'
+                            a=1
+                            break
+                        else :
+                            itemset_n.remove(item)
+                seq_new.append(itemset_n)
+                seq_new = list(filter(None, seq_new))
+            if seq_new != [] and seq_new != [['_']]:
+                db_object.append(seq_new)
+                print(seq_new)
+        print(db_object)
+                
+        if db_object !=[]:
+            find_frequent_item(db_object,min_supprot,prefix_item)
+        
+    return 0
+
+
+def writeTXT(t):
+    f = open('out.txt','w')
+    f.write(t)
+    f.close()
 
 def make_sequence(db):
     sequence_done = []
