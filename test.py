b3d121f232fb82f0af64be227e769c262aaa4495
Yi-Hsun Liu <lyharthur@gmail.com>
2016-08-02 16:49:05 +0800

    Update test.py

--- test.py
+++ test.py
@@ -4,8 +4,9 @@ from PrefixSpan import make_sequence ,find_frequent_item
 
 start = time.time()
 data= []
-
-with open('abc.txt') as f:
+filename1 = 'C50S10T2.5N10000.ascii'
+filename2 = 'abc.txt'
+with open(filename2) as f:
     for line in f:
         line = line.replace('\n','')
         line = line.split(' ')
@@ -13,10 +14,12 @@ with open('abc.txt') as f:
         data.append(line)
 
         
-f = open('sequent_done.txt', 'w')
+f = open('out.txt','w')
+f.write('')
+find_frequent_item(make_sequence(data),1,'')
 
-f.write(str(find_frequent_item(make_sequence(data),2,'')))
-f.close()
+
+
 
 end = time.time()
 elapsed = end - start
