e3195503f228ed31afd9eae6cd7ca7972504729d
Yi-Hsun Liu <lyharthur@gmail.com>
2016-08-04 15:19:03 +0800

    Update broken.py

--- broken.py
+++ broken.py
@@ -1,52 +1,52 @@
 from collections import defaultdict, namedtuple
+import time
 
-def find_frequent_item(db,min_supprot,seq_pattern):
+def find_frequent_item(db,min_support,seq_pattern):
+    
     prefix = defaultdict(lambda: 0)
     prefix_list = []
     last = seq_pattern[-1:]
-    
     #print(db,'start')
-    db_new = []
     prefix_out = ''
-    
+    #start1 = time.time()
     for sequence in db: #calculate prefix 
-        seq_new = []
         seq_done = defaultdict(lambda: 0)
         for itemset in sequence:           
-            itemset_new = itemset[:]
-            itemset_tmp = []
-            set_l = len(itemset)
             has_ = 0
-            for item in itemset:
+            has_last = 0
+            for index,item in enumerate(itemset):
+                if [item] == last:
+                    has_last =1
                 if item == '_':
-                    has_ = 1
-                for item1 in itemset:
+                    has_ = 1          
+                elif item != '_' :
+                    #for item1 in itemset[:index+1]:
                     itemnew = item + ')'
-                    #print( item ,item1 ,last)
-                    if ([item1] == last or item1 == '_' ) and prefix[itemnew] < min_supprot and seq_done[itemnew] == 0 and [item] != last:
-                        prefix[itemnew]+=1
+                    #print('lsitfor', item  ,last , index)
+                    if (has_last == 1 or has_ == 1 ) and prefix[itemnew] < min_support and seq_done[itemnew] == 0 and [item] != last:
+                        prefix[itemnew]+=1  # has )
                         seq_done[itemnew] = 1
-                        if prefix[itemnew] >= min_supprot:
+                        if prefix[itemnew] >= min_support:
                             prefix_list.append(itemnew)
-                    elif seq_done[item] == 0 and has_ == 0 and prefix[item] < min_supprot:
+                    elif seq_done[item] == 0 and has_ == 0 and prefix[item] < min_support:
                         prefix[item]+=1
                         seq_done[item] = 1
-                        if prefix[item] >= min_supprot:
+                        if prefix[item] >= min_support:
                             prefix_list.append(item)
 
-    if '_' in prefix_list:
-        prefix_list.remove('_')
-
-    if '_)' in prefix_list:
-        prefix_list.remove('_)')
 
 
-#if prefix_list != []:
-#       print(prefix_list,'plist')
+    #if prefix_list != []:
+    #    print(prefix_list,'plist')
+
+##    end1 = time.time()
+##    elapsed1 = end1 - start1
+##    print( "FOR(1) Time taken: ", elapsed1, "seconds.")
 
 
-    for prefix in prefix_list:  #create object_DB
-        db_object= []
+    #start2 = time.time()
+    for prefix in prefix_list:  #create probject_DB
+        db_project= []
         if seq_pattern ==[]:
             new = 1
         else:
@@ -70,39 +70,43 @@ def find_frequent_item(db,min_supprot,seq_pattern):
                             #print(has_,has_pre)
                             
                             if ((item == prefix[:-1] )or(item == prefix) ) and (has_== 1 or has_pre == 1 or new == 1):
-                                itemset_new[itemset_new.index(item)] = '_'
+                                itemset_new[itemset_new.index(item)] = '_'                           
                                 a=1
                                 break
                             else :
                                 itemset_new.remove(item)
+                                
                         else:
                             #print(item,prefix)
                             if item == '_' :
                                 has_ = 1
                             if seq_pattern[-2:-1] == [item]:
                                 has_pre = 1
-                                    #print(has_,has_pre)
+                            #print(has_,has_pre)
                             if ((item == prefix[:-1] )or(item == prefix)) and (has_== 0) :
                                 itemset_new[itemset_new.index(item)] = '_'
+                                
                                 a=1
                                 break
                             else :
-                                itemset_new.remove(item)
-
+                                itemset_new.remove(item)                                        
                 seq_new.append(itemset_new)
                 seq_new = list(filter(None, seq_new))
 
+
             if seq_new != [] and seq_new != [['_']]:
-                db_object.append(seq_new)
-                    #print(db_object)
-        #print(seq_pattern ,'out')
-        if db_object !=[]:
-            find_frequent_item(db_object,min_supprot,seq_pattern)
-
+                db_project.append(seq_new)
+        #print(db_project)
+        #print(seq_pattern,'out')
+        if db_project !=[]:
+            find_frequent_item(db_project,min_support,seq_pattern)
+        #print(seq_pattern,'out')
         writeTXT(str(seq_pattern)+'\n')
-
-        seq_pattern.pop()
         
+        seq_pattern.pop()
+##    end2 = time.time()
+##    elapsed2 = end2 - start2
+##    print( "FOR(2) Time taken: ", elapsed2, "seconds.")
         
     return 
 
@@ -111,6 +115,8 @@ def writeTXT(t):
     f = open('out.txt','a')
     f.write(t)
     f.close()
+
+
 
 def make_sequence(db):
     sequence_done = []
